3a3f0b41ea8cb86893dfb09bd3f04b68cdf1ae37
Wren McQueary <wren.mcqueary@gmail.com>
2025-06-01 15:45:15 -0400

    Another fix to velocity update in simulation.py

--- simulation.py
+++ simulation.py
@@ -26,11 +26,11 @@ def new_state(T, initial_physical_state: PhysicalState):
     Vf = v + (T/(m*r**2)) * dt
     theta_f = theta + Vf * dt
     if theta_f <= 0:
-        v *= -0.7
+        Vf *= -0.7
         #TODO play around with this value
         theta_f = 0
     if theta_f >= math.pi:
-        v *= -0.7
+        Vf *= -0.7
         theta_f = math.pi
     new_physical_state = PhysicalState(theta_f, Vf, m, r, dt)
     return new_physical_state
