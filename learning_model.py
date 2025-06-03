4daa60a1bc810880db7229b66039d1563927c4f0
Wren McQueary <wren.mcqueary@gmail.com>
2025-06-02 21:56:41 -0400

    Reduce motor strength to allow subtler actions

--- learning_model.py
+++ learning_model.py
@@ -1,14 +1,13 @@
 from collections import defaultdict
 from states import LearningState
 
+choices = [-1.5, -1, -0.5, 0, 0.5, 1, 1.5]
 
 def empty_choices():
-    choices = {-3: 0, -2: 0, -1: 0, 0: 0, 1: 0, 2: 0, 3: 0}
-    return choices
+    return {choice: 0 for choice in choices}
 
 def one_choices():
-    choices = {-3: 1, -2: 1, -1: 1, 0: 1, 1: 1, 2: 1, 3: 1}
-    return choices
+    return {choice: 1 for choice in choices}
 
 
 class LearningModel:
